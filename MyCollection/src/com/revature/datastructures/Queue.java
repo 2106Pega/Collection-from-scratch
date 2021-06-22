6fea6caeb206d8f93486a0b819406bf7c02ac939
chris-yarnot <chris@yarnot.com>
2021-06-22 16:04:25 -0500

    Revert "added a Queue implementation"
    This reverts commit d1d3d1e5e321bb2974a084c43e64ef60e7acde22.

--- MyCollection/src/com/revature/datastructures/Queue.java
+++ MyCollection/src/com/revature/datastructures/Queue.java
@@ -2,8 +2,8 @@ package com.revature.datastructures;
 
 public interface Queue<T> extends Collection<T> {
 	
-	public T pop();
+	public String pop();
 	
-	public T peek();
+	public String peek();
 
 }
